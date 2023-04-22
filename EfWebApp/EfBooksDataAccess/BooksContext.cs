727653d39cd630f5a1a034756fad96dfdd2b975c
Igor <montru@inbox.ru>
2023-04-23 00:33:19 +0200

    updated tables properties, added migrations

--- EfWebApp/EfBooksDataAccess/BooksContext.cs
+++ EfWebApp/EfBooksDataAccess/BooksContext.cs
@@ -5,8 +5,22 @@ namespace EfBooksDataAccess;
 
 public class BooksContext: DbContext
 {
-    public BooksContext(DbContextOptions options):base(options)
+    // public BooksContext(DbContextOptions options):base(options)
+    // {
+    // }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EfBooks;Integrated Security=true;");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().Property(nameof(Book.Title)).HasMaxLength(300);
+        modelBuilder.Entity<Author>().Property(nameof(Author.Name)).HasMaxLength(50);
+        modelBuilder.Entity<Member>().Property(nameof(Member.Name)).HasMaxLength(50);
+        modelBuilder.Entity<Member>().Property(nameof(Member.MemberId)).HasMaxLength(50);
     }
     
     public DbSet<Book> Books { get; set; }
--- EfWebApp/EfBooksModel/Models/Library/Author.cs
+++ EfWebApp/EfBooksModel/Models/Library/Author.cs
@@ -4,5 +4,5 @@ public class Author
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    
+    public List<Book> Books { get; set; } = null!;
 }
